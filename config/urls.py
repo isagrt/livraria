2b5a65c6db3c9a4e821555abf619d039b62d466a
isagrt <isadoragoedert05@gmail.com>
2023-04-25 15:59:07 -0300

    criação dos serializers

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,15 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
+router.register(r"autor", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livro", LivroViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("", include(router.urls)),
 ]
